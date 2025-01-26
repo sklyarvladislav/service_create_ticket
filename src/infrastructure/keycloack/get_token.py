f492b92220e41dc61cc9192756b029ad54ada233
Владислав Скляренко <sklyarvladislavtl@gmail.com>
2025-01-26 16:58:36 +0300

    handle get token

--- src/infrastructure/keycloack/get_token.py
+++ src/infrastructure/keycloack/get_token.py
@@ -1,27 +1,25 @@
-from fastapi import HTTPException
 from cryptography.fernet import Fernet
 import httpx
 
 from src.infrastructure.configs import load_config
+from src.presentation.exceptions import handle_exceptions, CustomException
 
 config = load_config()
 decoder = Fernet(key=config.fernet_key)
 
 
+@handle_exceptions([httpx.RequestError, httpx.HTTPStatusError], CustomException)
 async def get_access_token() -> str:
     async with httpx.AsyncClient() as client:
-        try:
-            response = await client.post(
-                config.auth_url,
-                data={
-                    "grant_type": "client_credentials",
-                    "client_id": config.client_id,
-                    "client_secret": decoder.decrypt(
-                        config.client_secret.encode()
-                    ).decode(),
-                },
-            )
-            response.raise_for_status()
-            return response.json()["access_token"]
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=f"error get access_token: {e}")
+        response = await client.post(
+            config.auth_url,
+            data={
+                "grant_type": "client_credentials",
+                "client_id": config.client_id,
+                "client_secret": decoder.decrypt(
+                    config.client_secret.encode()
+                ).decode(),
+            },
+        )
+        response.raise_for_status()
+        return response.json()["access_token"]
